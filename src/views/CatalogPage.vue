0504c95ce49c7143cb81cec553239ac7828d852a
kliuevia <i.kliuev@haulmont.com>
2024-06-18 17:13:55 +0400

    upd

--- src/views/CatalogPage.vue
+++ src/views/CatalogPage.vue
@@ -10,7 +10,7 @@
       <ul class="catalog-page__nav-list">
         <li
           :class="{
-            'is-all-new-active': allNewView === true
+            'is-all-new-active': allNewView === true,
           }"
           class="catalog-page__nav-list-item"
           @click="openAllNew"
@@ -19,7 +19,7 @@
         </li>
         <li
           :class="{
-            'is-fruits-active': fruitsView === true
+            'is-fruits-active': fruitsView === true,
           }"
           class="catalog-page__nav-list-item"
           @click="openFruits"
@@ -28,7 +28,7 @@
         </li>
         <li
           :class="{
-            'is-candies-active': candiesView === true
+            'is-candies-active': candiesView === true,
           }"
           class="catalog-page__nav-list-item"
           @click="openCandies"
@@ -37,7 +37,7 @@
         </li>
         <li
           :class="{
-            'is-berries-active': berriesView === true
+            'is-berries-active': berriesView === true,
           }"
           class="catalog-page__nav-list-item"
           @click="openBerries"
@@ -46,7 +46,7 @@
         </li>
         <li
           :class="{
-            'is-nuts-active': nutsView === true
+            'is-nuts-active': nutsView === true,
           }"
           class="catalog-page__nav-list-item"
           @click="openNuts"
@@ -55,7 +55,7 @@
         </li>
         <li
           :class="{
-            'is-dried-active': driedFruitsView === true
+            'is-dried-active': driedFruitsView === true,
           }"
           class="catalog-page__nav-list-item"
           @click="openDries"
@@ -63,34 +63,109 @@
           Сухофрукты
         </li>
       </ul>
+      <div class="catalog-page__price-filter">
+        <span class="catalog-page__price-filter-span">Цена:</span>
+        <button
+          :class="{ 'is-selected': selectedPriceRange.value === 'any' }"
+          @click="filterByPrice(0, 1000000, 'any')"
+        >
+          Любая цена
+        </button>
+        <button
+          :class="{ 'is-selected': selectedPriceRange.value === '1500-2000' }"
+          @click="filterByPrice(1500, 2000, '1500-2000')"
+        >
+          1500-2000
+        </button>
+        <button
+          :class="{ 'is-selected': selectedPriceRange.value === '2000-3000' }"
+          @click="filterByPrice(2000, 3000, '2000-3000')"
+        >
+          2000-3000
+        </button>
+        <button
+          :class="{ 'is-selected': selectedPriceRange.value === '3000-5000' }"
+          @click="filterByPrice(3000, 5000, '3000-5000')"
+        >
+          3000-5000
+        </button>
+      </div>
     </div>
 
     <div class="catalog-page__products-container">
-      <ProductCard></ProductCard>
+      <ProductCard
+        v-for="product in filteredProducts"
+        :key="product.id"
+        :product="product"
+      />
     </div>
   </div>
 </template>
 <script setup lang="ts">
-import { ref, computed, onMounted } from 'vue';
-import ProductCard from '@/components/ProductCard.vue'
-import { useMainStore } from '@/stores/index'
-import { VBtn } from 'vuetify/components'
+import { ref, computed, onMounted } from "vue";
+import ProductCard from "@/components/ProductCard.vue";
+import { useMainStore } from "@/stores/index";
+import { VBtn } from "vuetify/components";
 
-const store = useMainStore()
+const store = useMainStore();
 
 onMounted(async () => {
-    store.loadProductsFromBackend();
-})
+  store.loadProductsFromBackend();
+  filterByPrice(0, 1000000);
+});
 
-const allNewView = ref(true)
-const fruitsView = ref(false)
-const candiesView = ref(false)
-const berriesView = ref(false)
-const nutsView = ref(false)
-const driedFruitsView = ref(false)
+const priceFrom = ref<number | null>(null);
+const priceTo = ref<number | null>(null);
+const selectedPriceRange = ref('any');
 
-const priceFrom = ref('')
-const priceTo = ref('')
+const filterByPrice = (minPrice: number, maxPrice: number, range: string) => {
+  priceFrom.value = minPrice;
+  priceTo.value = maxPrice;
+    selectedPriceRange.value = range;
+};
+
+const filteredProducts = computed(() => {
+  if (priceFrom.value !== null && priceTo.value !== null) {
+    if (allNewView.value) {
+      return store.allNew.filter(
+        (product) => product.price >= priceFrom.value && product.price <= priceTo.value
+      );
+    }
+    if (fruitsView.value) {
+      return store.fruits.filter(
+        (product) => product.price >= priceFrom.value && product.price <= priceTo.value
+      );
+    }
+    if (candiesView.value) {
+      return store.candies.filter(
+        (product) => product.price >= priceFrom.value && product.price <= priceTo.value
+      );
+    }
+    if (berriesView.value) {
+      return store.berries.filter(
+        (product) => product.price >= priceFrom.value && product.price <= priceTo.value
+      );
+    }
+    if (nutsView.value) {
+      return store.nuts.filter(
+        (product) => product.price >= priceFrom.value && product.price <= priceTo.value
+      );
+    }
+    if (driedFruitsView.value) {
+      return store.driedFruits.filter(
+        (product) => product.price >= priceFrom.value && product.price <= priceTo.value
+      );
+    }
+  }
+  return store.products;
+});
+
+const allNewView = ref(true);
+const fruitsView = ref(false);
+const candiesView = ref(false);
+const berriesView = ref(false);
+const nutsView = ref(false);
+const driedFruitsView = ref(false);
 
 const openAllNew = () => (
   ((allNewView.value = true), store.doAllNewView(true)),
@@ -99,7 +174,7 @@ const openAllNew = () => (
   ((berriesView.value = false), store.doBerriesView(false)),
   ((nutsView.value = false), store.doNutsView(false)),
   ((driedFruitsView.value = false), store.doDriedFruitsView(false))
-)
+);
 
 const openFruits = () => (
   ((allNewView.value = false), store.doAllNewView(false)),
@@ -108,7 +183,7 @@ const openFruits = () => (
   ((berriesView.value = false), store.doBerriesView(false)),
   ((nutsView.value = false), store.doNutsView(false)),
   ((driedFruitsView.value = false), store.doDriedFruitsView(false))
-)
+);
 
 const openCandies = () => (
   ((allNewView.value = false), store.doAllNewView(false)),
@@ -117,7 +192,7 @@ const openCandies = () => (
   ((berriesView.value = false), store.doBerriesView(false)),
   ((nutsView.value = false), store.doNutsView(false)),
   ((driedFruitsView.value = false), store.doDriedFruitsView(false))
-)
+);
 
 const openBerries = () => (
   ((allNewView.value = false), store.doAllNewView(false)),
@@ -126,7 +201,7 @@ const openBerries = () => (
   ((berriesView.value = true), store.doBerriesView(true)),
   ((nutsView.value = false), store.doNutsView(false)),
   ((driedFruitsView.value = false), store.doDriedFruitsView(false))
-)
+);
 
 const openNuts = () => (
   ((allNewView.value = false), store.doAllNewView(false)),
@@ -135,7 +210,7 @@ const openNuts = () => (
   ((berriesView.value = false), store.doBerriesView(false)),
   ((nutsView.value = true), store.doNutsView(true)),
   ((driedFruitsView.value = false), store.doDriedFruitsView(false))
-)
+);
 
 const openDries = () => (
   ((allNewView.value = false), store.doAllNewView(false)),
@@ -144,7 +219,7 @@ const openDries = () => (
   ((berriesView.value = false), store.doBerriesView(false)),
   ((nutsView.value = false), store.doNutsView(false)),
   ((driedFruitsView.value = true), store.doDriedFruitsView(true))
-)
+);
 </script>
 <style lang="scss">
 .is-all-new-active,
@@ -163,7 +238,7 @@ const openDries = () => (
   min-width: 300px;
 }
 .catalog-page {
-  margin-top: 150px;                                 
+  margin-top: 150px;
   color: #411212;
   display: flex;
   flex-direction: column;
@@ -187,7 +262,7 @@ const openDries = () => (
     .catalog-page__nav-title,
     .catalog-page__price-title {
       font-size: 32px;
-      font-family: 'YanoneKaffeesatz-regular';
+      font-family: "YanoneKaffeesatz-regular";
       color: #411212;
     }
     .catalog-page__nav-list {
@@ -195,7 +270,7 @@ const openDries = () => (
       gap: 50px;
       .catalog-page__nav-list-item {
         font-size: 24px;
-        font-family: 'YanoneKaffeesatz-regular';
+        font-family: "YanoneKaffeesatz-regular";
         text-transform: uppercase;
         cursor: pointer;
       }
@@ -208,8 +283,22 @@ const openDries = () => (
       .catalog-page__price-filter-span {
         font-size: 32px;
         color: #411212;
-        font-family: 'YanoneKaffeesatz-regular';
+        font-family: "YanoneKaffeesatz-regular";
       }
+
+      button {
+        background-color: #f0f0f0;
+        border: none;
+        padding: 10px 20px;
+        cursor: pointer;
+        font-family: "YanoneKaffeesatz-regular";
+        font-size: 16px;
+      }
+
+      button:hover {
+        background-color: #e0e0e0;
+      }
+
       .catalog-page__price-filter-input {
         min-width: 100px;
       }
